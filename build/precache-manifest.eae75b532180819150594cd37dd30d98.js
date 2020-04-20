9197969f99b3ee155f69e111c32d355e945a1f26
Michael Nguyen <vitawebsitedesign@github.com>
2020-04-20 14:19:59 +1000

    rebuild

---
+++ build/precache-manifest.eae75b532180819150594cd37dd30d98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbae91ac8044f974f97ac3ba2f0814a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a55bed2053ecf86fe2e5",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "421c9eb3da22821cee53",
+    "url": "./static/js/2.0e12151a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0e12151a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55bed2053ecf86fe2e5",
+    "url": "./static/js/main.d59a30b3.chunk.js"
+  },
+  {
+    "revision": "c0df23531557ae637560",
+    "url": "./static/js/runtime-main.46e6e4a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.308e6d008f771709487e3053a5df0f5a.js"
+  "./precache-manifest.eae75b532180819150594cd37dd30d98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
