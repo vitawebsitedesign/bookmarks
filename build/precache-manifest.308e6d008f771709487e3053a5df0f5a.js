8795c2c533539578ae1c137558748af9e3e4c6d9
Michael Nguyen <vitawebsitedesign@github.com>
2020-04-20 14:10:12 +1000

    add build folder

---
+++ build/precache-manifest.308e6d008f771709487e3053a5df0f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc54d8f9c0acc7901881088ccc0b9d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce2dec4457416ba0414",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "421c9eb3da22821cee53",
+    "url": "/static/js/2.0e12151a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e12151a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce2dec4457416ba0414",
+    "url": "/static/js/main.92e417c0.chunk.js"
+  },
+  {
+    "revision": "a7719f62bb150f0dcce0",
+    "url": "/static/js/runtime-main.84cad9a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
