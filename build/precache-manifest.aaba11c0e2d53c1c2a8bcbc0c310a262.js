e17a07d5b23761245049416dc862828424335812
Michael Nguyen <vitawebsitedesign@github.com>
2020-04-23 01:00:33 +1000

    build

---
+++ build/precache-manifest.aaba11c0e2d53c1c2a8bcbc0c310a262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aed07263737c2567063bbe024c9046",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36269ae2581d2cfb526e",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db0f3df5fba6d4bf7ebd",
+    "url": "./static/js/2.ed676c5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ed676c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36269ae2581d2cfb526e",
+    "url": "./static/js/main.77b0b511.chunk.js"
+  },
+  {
+    "revision": "92b30749caf11ea26fc7",
+    "url": "./static/js/runtime-main.6e7ddb13.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eae75b532180819150594cd37dd30d98.js"
+  "./precache-manifest.aaba11c0e2d53c1c2a8bcbc0c310a262.js"
 );
 
 self.addEventListener('message', (event) => {
