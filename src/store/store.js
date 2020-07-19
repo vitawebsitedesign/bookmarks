66bb8987a29305b6b3d183f67cad40391e36e7cd
Michael Nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2020-07-19 15:20:35 +1000

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,21 +3,25 @@ import {addPortfolioItems} from '../actions/portfolio-items';
 import AgileAES from '../portfolio-items/agile-aes';
 import AsxMarketCapitalisation from '../portfolio-items/asx-market-capitalisation';
 import BngCampaignXmlGenerator from '../portfolio-items/bng-campaign-xml-generator';
+import BulkUrlShortener from '../portfolio-items/bulk-url-shortener';
 import FollowTheProcess from '../portfolio-items/follow-the-process';
 import MachiKoro from '../portfolio-items/machi-koro';
 import Mint from '../portfolio-items/mint';
 import ModernBng from '../portfolio-items/modern-bng';
 import RealestateVisualiser from '../portfolio-items/realestate-visualiser';
-import SqlKeywordFormatter from '../portfolio-items/sql-keyword-formatter.js';
+import Sudoku from '../portfolio-items/sudoku';
+import SqlKeywordFormatter from '../portfolio-items/sql-keyword-formatter';
 import portfolioItems from '../reducers/portfolio-items';
 
 const store = createStore(portfolioItems);
 store.dispatch(addPortfolioItems([
     ModernBng,
+    Sudoku,
     BngCampaignXmlGenerator,
-    SqlKeywordFormatter,
-    Mint,
     AgileAES,
+    Mint,
+    SqlKeywordFormatter,
+    BulkUrlShortener,
     FollowTheProcess,
     MachiKoro,
     RealestateVisualiser,
